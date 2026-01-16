e8d05ab078945393d0aca348517c4414ad65672e
mrinal <mrinalchandaml2@gmail.com>
2026-01-17 01:19:20 +0530

    P13.java

--- src/CountPrime204.java
+++ src/CountPrime204.java
@@ -1,5 +1,26 @@
 public class CountPrime204 {
+    public static int countPrimes(int n) {
+        if (n <= 1) {
+            return 0;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            int count = 0;
+            for (int i = 3; i <= n; i++) {
+                count += checkPrime(i);
+            }
+            return count;
+        }
+    }
+    public static int checkPrime(int i) {
+        for(int j = 2; j <= i; j++) {
+            if(i % j == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
     public static void main(String[] args) {
-
+        System.out.println(countPrimes(5));
     }
 }
--- src/P13.java
+++ src/P13.java
@@ -1,8 +1,16 @@
 public class P13 {
     public static void main(String[] args) {
         int n = 5;
+        int first = 0;
+        int second = 1;
         for(int i = 1; i<= n; i++) {
-
+            for(int j = 1; j <= i; j++ ) {
+                System.out.print(first+" ");
+                int next = first + second;
+                first = second;
+                second = next;
+            }
+            System.out.println();
         }
     }
 }
